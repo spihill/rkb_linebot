88a4b62d2fdbd99a5899d17848eedcb2637c1eaf
spihill <spihill.in@gmail.com>
2019-07-23 19:57:09 +0900

    オウム返し

--- index.js
+++ index.js
@@ -32,14 +32,10 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
-                }));
-            }
+            events_processed.push(bot.replyMessage(event.replyToken, {
+                type: "text",
+                text: event.message.text
+            }));
         }
     });
 
