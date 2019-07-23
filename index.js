0148c220c8963ccf04fb2d271b61e1b56d350cc9
spihill <spihill.in@gmail.com>
2019-07-23 21:24:16 +0900

    ikare

--- index.js
+++ index.js
@@ -31,11 +31,14 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
-            events_processed.push(bot.replyMessage(event.replyToken, {
-                type: "text",
-                text: event.message.text
-            }));
+        if (event.type === "message" && event.message.type === "text"){
+            if (event.message.text.indexOf("いかれ") >= 0) {
+                let reply = event.massage.text.replace(/いかれ/g/, "カレー");
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: reply 
+                }));
+            }
         }
     });
 
@@ -46,5 +49,3 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
         }
     );
 });
-
-
