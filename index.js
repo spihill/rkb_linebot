75de7e389f3f00d88a33e53e04e916cba7f8d08f
spihill <spihill.in@gmail.com>
2019-07-27 11:23:37 +0900

    mecab try

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 // モジュールのインポート
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const mecab = new require("mecab-asynx");
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+	channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+	channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
 };
 
 // -----------------------------------------------------------------------------
@@ -22,30 +23,39 @@ const bot = new line.Client(line_config);
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
-    // 先行してLINE側にステータスコード200でレスポンスする。
-    res.sendStatus(200);
+	// 先行してLINE側にステータスコード200でレスポンスする。
+	res.sendStatus(200);
 
-    // すべてのイベント処理のプロミスを格納する配列。
-    let events_processed = [];
+	// すべてのイベント処理のプロミスを格納する配列。
+	let events_processed = [];
 
-    // イベントオブジェクトを順次処理。
-    req.body.events.forEach((event) => {
-        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type === "message" && event.message.type === "text"){
-            if (event.message.text.indexOf("いかれ") >= 0) {
-                let reply = event.message.text.replace(/いかれ/g, "カレー") + "ということですか？";
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: reply 
-                }));
-            }
-        }
-    });
+	mecab.command = "mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd";
 
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
+	// イベントオブジェクトを順次処理。
+	req.body.events.forEach((event) => {
+		// この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+		if (event.type === "message" && event.message.type === "text"){
+//			if (event.message.text.indexOf("いかれ") >= 0) {
+//				let reply = event.message.text.replace(/いかれ/g, "カレー") + "ということですか？";
+			let replymessage = "";
+			mecab.parseFormat(event.message.text, function(err, morphs) {
+				if (err) throw err;
+				morphs.map(function(morph) {
+					replymessage += morph.pronunciation;
+				});
+			});
+			events_processed.push(bot.replyMessage(event.replyToken, {
+				type: "text",
+				text: replymessage
+			}));
+			
+		}
+	});
+
+	// すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+	Promise.all(events_processed).then(
+		(response) => {
+			console.log(`${response.length} event(s) processed.`);
+		}
+	);
 });
